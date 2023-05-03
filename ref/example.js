d64e648cdb5440abd98e22b5738ff73de95637f8
노다설 <sia0940@gmail.com>
2023-05-03 21:04:29 +0900

    add. html,css 기초

---
+++ ref/example.js
@@ -0,0 +1,15 @@
+//함수 선언
+function hello(name){
+    alert(name+" 안녕")
+}
+
+//함수 표현식
+const hello = function(name){
+    alert(name+" 안녕")
+}
+
+//화살표 함수
+const hello = (name) => {
+    alert(name+" 안녕")
+}
+
